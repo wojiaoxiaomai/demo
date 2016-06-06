5dcde89a23f3b4c40f75568c2d802634be5e3e22
wojiaoxiaomai <767188110@qq.com>
2016-06-06 18:02:30 +0800

    测试

--- app/src/main/java/com/lcdq/github/MainActivity.java
+++ app/src/main/java/com/lcdq/github/MainActivity.java
@@ -10,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "nihao", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "11111111", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "222222222", Toast.LENGTH_SHORT).show();
+
     }
 }
