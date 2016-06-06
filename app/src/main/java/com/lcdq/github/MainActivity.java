8aac30de14e6e1fb1834b12e0a3ea0e5f0621242
wojiaoxiaomai <767188110@qq.com>
2016-06-06 17:34:23 +0800

    测试

--- app/src/main/java/com/lcdq/github/MainActivity.java
+++ app/src/main/java/com/lcdq/github/MainActivity.java
@@ -2,7 +2,6 @@ package com.lcdq.github;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "pxz123", Toast.LENGTH_SHORT).show();
     }
 }
